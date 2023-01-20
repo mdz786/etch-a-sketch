43d231938b55dd5737ec21446f8c9234c0e21036
mdz786 <mohd17zeeshan17@gmail.com>
2023-01-20 21:32:44 +0530

    prompt for color

--- index.js
+++ index.js
@@ -9,36 +9,36 @@ function createGrid(x){
             const divs = document.createElement('div');
             divs.classList = 'div';
             miniContainers.appendChild(divs);
-            divs.addEventListener('mouseover',() => divs.style.background = 'gold');
+            divs.addEventListener('mouseover',() => divs.style.background = 'red');
             if(x==10){
-                divs.setAttribute('style','height: 97.5px;width: 100px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 97.5px;width: 100px;border:0.1px solid white;');
             }
             else if(x==20){
-                divs.setAttribute('style','height: 47.5px;width: 50px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 47.5px;width: 50px;border:0.1px solid white;');
             }
             else if(x==30){
-                divs.setAttribute('style','height: 30.9px;width: 33.3px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 30.9px;width: 33.3px;border:0.1px solid white;');
             }
             else if(x==40){
-                divs.setAttribute('style','height: 22.5px;width: 25px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 22.5px;width: 25px;border:0.1px solid white;');
             }
             else if(x==50){
-                divs.setAttribute('style','height: 17.5px;width: 20px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 17.5px;width: 20px;border:0.1px solid white;');
             }
             else if(x==60){
-                divs.setAttribute('style','height: 14.2px;width: 16.66px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 14.2px;width: 16.66px;border:0.1px solid white;');
             }
             else if(x==70){
-                divs.setAttribute('style','height: 11.8px;width: 14.2px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 11.8px;width: 14.2px;border:0.1px solid white;');
             }
             else if(x==80){
-                divs.setAttribute('style','height: 10px;width: 12.5px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 10px;width: 12.5px;border:0.1px solid white;');
             }
             else if(x==90){
-                divs.setAttribute('style','height: 8.63px;width: 11.11px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 8.63px;width: 11.11px;border:0.1px solid white;');
             }
             else if(x==100){
-                divs.setAttribute('style','height: 7.5px;width: 7.5px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+                divs.setAttribute('style','height: 7.5px;width: 7.5px;border:0.1px solid white;');
             }
             
         }  
@@ -48,7 +48,7 @@ function createGrid(x){
 }
 
 function gridSize(){
-    return prompt("enter the size of the grid in multiples of 10...!");
+    return prompt("enter the pen size in multiples of 10...!");
 }
 
 function Main(a,b){

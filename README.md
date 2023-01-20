64eb03bb9fd2fc58b40de208740058c83f903592
mdz786 <mohd17zeeshan17@gmail.com>
2023-01-20 18:11:23 +0530

    should add prompt for color

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+
+1 > This is simple version of the Paint(without any tools) in Windows.
+2 > This project is to improve my DOM manipulation ability.
---
+++ index.js
@@ -0,0 +1,65 @@
+const container = document.querySelector('.container');
+
+function createGrid(x){
+    for(let i=1;i<=x;i++){
+        const miniContainers = document.createElement('div');
+        container.appendChild(miniContainers);
+        miniContainers.style.display='flex';
+        for(let j=1;j<=x;j++){
+            const divs = document.createElement('div');
+            divs.classList = 'div';
+            miniContainers.appendChild(divs);
+            divs.addEventListener('mouseover',() => divs.style.background = 'gold');
+            if(x==10){
+                divs.setAttribute('style','height: 97.5px;width: 100px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==20){
+                divs.setAttribute('style','height: 47.5px;width: 50px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==30){
+                divs.setAttribute('style','height: 30.9px;width: 33.3px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==40){
+                divs.setAttribute('style','height: 22.5px;width: 25px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==50){
+                divs.setAttribute('style','height: 17.5px;width: 20px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==60){
+                divs.setAttribute('style','height: 14.2px;width: 16.66px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==70){
+                divs.setAttribute('style','height: 11.8px;width: 14.2px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==80){
+                divs.setAttribute('style','height: 10px;width: 12.5px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==90){
+                divs.setAttribute('style','height: 8.63px;width: 11.11px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            else if(x==100){
+                divs.setAttribute('style','height: 7.5px;width: 7.5px;border:0.1px solid rgba(0, 0, 0, 0.404);');
+            }
+            
+        }  
+    }
+    
+    
+}
+
+function gridSize(){
+    return prompt("enter the size of the grid in multiples of 10...!");
+}
+
+function Main(a,b){
+    let gridSizeValue = a();
+    b(gridSizeValue);
+}
+
+
+Main(gridSize,createGrid);
+
+
+
+
+
